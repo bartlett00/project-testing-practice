42f19040b65265b05b96ea9a6a7534bb839a4dde
Pax <113464089+bartlett00@users.noreply.github.com>
2024-10-23 13:30:13 -0500

    Complete calculator.js and respective testing suite.

--- calculator.js
+++ calculator.js
@@ -2,6 +2,15 @@ let calculator = {
   add: (x, y) => {
     return x + y;
   },
+  subtract: (x, y) => {
+    return x - y;
+  },
+  divide: (x, y) => {
+    return x / y;
+  },
+  multiply: (x, y) => {
+    return x * y;
+  },
 };
 
 export default calculator;
--- calculator.test.js
+++ calculator.test.js
@@ -3,5 +3,32 @@ import calculator from "./calculator";
 test("calculator obj contains an add method", () => {
   expect(calculator.add).toBeDefined();
 });
-
-test("calculator obj contains a subtract method", () => {});
+test("calculator obj contains a subtract method", () => {
+  expect(calculator.subtract).toBeDefined();
+});
+test("calculator obj contains a divide method", () => {
+  expect(calculator.divide).toBeDefined();
+});
+test("calculator obj contains a multiply method", () => {
+  expect(calculator.multiply).toBeDefined();
+});
+test("add method returns sum of two numbers", () => {
+  let actual = calculator.add(2, 2);
+  let expected = 4;
+  expect(actual).toBe(expected);
+});
+test("subtract method returns x - y", () => {
+  let actual = calculator.subtract(2, 2);
+  let expected = 0;
+  expect(actual).toBe(expected);
+});
+test("divide method returns x/y", () => {
+  let actual = calculator.divide(12, 2);
+  let expected = 6;
+  expect(actual).toBe(expected);
+});
+test("multiply method returns product of x and y", () => {
+  let actual = calculator.multiply(5, 4);
+  let expected = 20;
+  expect(actual).toBe(expected);
+});
