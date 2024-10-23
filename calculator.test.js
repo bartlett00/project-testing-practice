4bed5d43ab858dfe66bcac838891ad664289284c
Pax <113464089+bartlett00@users.noreply.github.com>
2024-10-23 11:54:08 -0500

    Begin calculator object tests.

---
+++ calculator.test.js
@@ -0,0 +1,7 @@
+import calculator from "./calculator";
+
+test("calculator obj contains an add method", () => {
+  expect(calculator.add).toBeDefined();
+});
+
+test("calculator obj contains a subtract method", () => {});
