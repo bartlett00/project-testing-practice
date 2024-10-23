5d32ecd4a17d2d79a21ea1e432b2230305af5af5
Pax <113464089+bartlett00@users.noreply.github.com>
2024-10-23 11:42:51 -0500

    Complete functions and simple test suites for capitalize.js and reverseString.js.

--- capitalize.js
+++ capitalize.js
@@ -1,6 +1,6 @@
 function capitalize(string) {
   let stringParsed = string.split("");
-  stringParsed[0].toUpperCase();
+  stringParsed[0] = stringParsed[0].toUpperCase();
   return stringParsed.join("");
 }
 
