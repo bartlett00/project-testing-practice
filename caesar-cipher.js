b2ead4ec5854a8520dab2bb1c4dd287f4f384d85
Pax <113464089+bartlett00@users.noreply.github.com>
2024-10-24 10:17:05 -0500

    Complete functionality of caesar-cipher.js, passing all required tests.

--- caesar-cipher.js
+++ caesar-cipher.js
@@ -1,41 +1,43 @@
 export default function caesarCipher(string, shift) {
-  let alphabetRegex = /^[A-Za-z]+$/;
   let stringSplit = [...string];
-  let alphabetKey = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-  ];
-
-  let cipher = stringSplit.map((character) => {
-    if (character.match(alphabetRegex)) {
-      let shiftedChar = shiftLetter(character, shift);
-    }
+  let cipher = [];
+  stringSplit.map((character) => {
+    cipher.push(shiftLetter(character, shift));
   });
-  return cipher;
+  return cipher.join("");
 }
 
-function shiftLetter(character, shift) {}
+// A-Z char. codes are 65 - 90
+// a-z char. codes are 97 - 122
+function shiftLetter(character, shift) {
+  let charCode = character.charCodeAt(0);
+  let shifted = charCode + shift;
+  let newCharCode = character.charCodeAt(0);
+  //check if character is A-Z
+  if (charCode >= 65 && charCode <= 90) {
+    //check if shift needs to "wrap" forwards
+    if (shifted > 90) {
+      let difference = shifted - 90;
+      newCharCode = 64 + difference;
+      //check if shift needs to "wrap" backwards
+    } else if (shifted < 65) {
+      let difference = shifted - 65;
+      newCharCode = 89 + difference;
+      //otherwise, simply shift char. code
+    } else {
+      newCharCode = shifted;
+    }
+    //same logic as before, but for other character range
+  } else if (charCode >= 97 && charCode <= 122) {
+    if (shifted > 122) {
+      let difference = shifted - 122;
+      newCharCode = 96 + difference;
+    } else if (shifted < 97) {
+      let difference = shifted - 97;
+      newCharCode = 121 + difference;
+    } else {
+      newCharCode = shifted;
+    }
+  }
+  return String.fromCharCode(newCharCode);
+}
--- caesar-cipher.test.js
+++ caesar-cipher.test.js
@@ -1,12 +1,24 @@
 import caesarCipher from "./caesar-cipher";
 test("function takes string and shift factor, returning the shifted string.", () => {
-  let actual = caesarCipher("hello world!", 4);
-  let expected = "lipps asvph!";
+  let actual = caesarCipher("hello", 4);
+  let expected = "lipps";
   expect(actual).toBe(expected);
 });
 
 test("function correctly wraps the cipher from z -> a", () => {
   let actual = caesarCipher("xyz", 3);
   let expected = "abc";
+  expect(actual).toBe(expected);
+});
+
+test("function preserves character cases.", () => {
+  let actual = caesarCipher("HeLLo", 3);
+  let expected = "KhOOr";
+  expect(actual).toBe(expected);
+});
+
+test("function preserves punctuation, spaces, and non-alphabetical characters.", () => {
+  let actual = caesarCipher("Hello, World!", 3);
+  let expected = "Khoor, Zruog!";
   expect(actual).toBe(expected);
 });
